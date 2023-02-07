3ba85c0ac0512aee99803d819a05b649908e476e
Joel Cormack <dgowellkomrak@gmail.com>
2023-02-07 13:28:45 -0600

    add responsive footer

--- footer.php
+++ footer.php
@@ -13,53 +13,42 @@
 </div><!-- #page .container -->
 
 <footer id="colophon" class="footer">
-    <div class="footer__column">
-        <div class="footer__img">
-            <img class="footer__logo" src="" />
-        </div>
-        <div class="footer__social-links">
-            <div class="footer__logo">
-                <a href="https://twitter.com/meaplatform">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/twitter-icon.svg"
-                        alt="Twitter Logo" />
-                </a>
-            </div>
-            <div class="footer__social-logo">
-                <a href="#">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/facebook-logo.svg"
-                        alt="Facebook Logo" />
-                </a>
-            </div>
-            <div class="footer__social-logo">
-                <a href="https://www.linkedin.com/company/meaplatform">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/linkedin-logo.svg"
-                        alt="LinkedIn Logo" />
-                </a>
-            </div>
-            <div class="footer__social-logo">
-                <a href="https://www.linkedin.com/company/meaplatform">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/linkedin-logo.svg"
-                        alt="LinkedIn Logo" />
-                </a>
-            </div>
-        </div>
-        <?php if (is_active_sidebar( 'footer-1' )) : ?>
-        <h2 class="screen-reader-text"><?php esc_html_e( 'Footer', 'genesis-block-theme' ); ?></h2>
-        <?php if ( is_active_sidebar( 'footer-1' ) ) { ?>
-        <div class="footer__column">
-            <?php dynamic_sidebar( 'footer-1' ); ?>
-        </div>
-        <?php } ?>
-
-
-        <?php endif; ?>
-
-        <div class="footer-bottom">
-            <div class="footer-tagline">
-                <div class="site-info">
-                    <?php echo genesis_block_theme_filter_footer_text(); ?>
+    <div class="footer__container">
+        <div class="footer__column footer__column--social-links">
+            <img class="footer__logo" alt="Aspectus Logo"
+                src="<?php echo get_stylesheet_directory_uri() ?>/assets/aspectus-logo.svg" />
+            <div class="footer__social-links">
+                <div class="footer__social-logo">
+                    <a href="#">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/linkedin-logo.svg"
+                            alt="LinkedIn Logo" />
+                    </a>
                 </div>
-            </div><!-- .footer-tagline -->
+                <div class="footer__social-logo">
+                    <a href="#">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/facebook-logo.svg"
+                            alt="Facebook Logo" />
+                    </a>
+                </div>
+                <div class="footer__social-logo">
+                    <a href="#">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/linkedin-logo.svg"
+                            alt="LinkedIn Logo" />
+                    </a>
+                </div>
+                <div class="footer__social-logo">
+                    <a href="#">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/twitter-icon.svg"
+                            alt="Instagram Logo" />
+                    </a>
+                </div>
+            </div>
+            <div class="footer__tag-line">
+                A Formula for Success.
+            </div>
+        </div><!--footer column-->
+        <div class="footer__column footer__column--menu">
+            <h3 class="footer__title">Important Links</h3>
             <?php if ( has_nav_menu( 'footer' ) ) { ?>
             <nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'genesis-block-theme' ); ?>">
                 <?php
@@ -73,7 +62,16 @@
                     ?>
             </nav><!-- .footer-navigation -->
             <?php } ?>
-        </div><!-- .footer-bottom -->
+        </div><!--footer column-->
+        <div class="footer__column footer__column--contact-us">
+            <h3 class="footer__title">Contact Us</h3>
+            <p class="footer__text">We are an independent global brand, marketing and communications agency with offices
+                in London, New York, Aberdeen, Singapore and Luzern.</p>
+            <div class="footer__button-container">
+                <button class="footer__button button">Contact</button>
+            </div>
+        </div><!--footer column-->
+    </div><!-- footer container -->
 </footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
