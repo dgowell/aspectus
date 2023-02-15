bc70798c68e60593caca774c60fadfbc6b902443
Joel Cormack <dgowellkomrak@gmail.com>
2023-02-15 10:32:38 -0600

    feature/style slide indicator

--- js/aspectus-sliders.js
+++ js/aspectus-sliders.js
@@ -1,9 +1,17 @@
 jQuery(document).ready(function () {
-    jQuery('.slick-slider ul').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: jQuery('.js-slider-prev'),
-        nextArrow: jQuery('.js-slider-next'),
+  jQuery('.slick-slider ul').slick({
+    infinite: !0,
+    dots: !0,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: jQuery('.js-slider-prev'),
+    nextArrow: jQuery('.js-slider-next'),
+    dotsClass: "js-client-slider__indicators",
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: { slidesToShow: 1, slidesToScroll: 1 }
+      }
+    ]
   });
 });
