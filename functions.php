be00495dffe1a98c1418afc5ff9c2059c2c5954d
Joel Cormack <dgowellkomrak@gmail.com>
2023-02-14 18:22:17 -0600

    feature/add slider logos and text

--- functions.php
+++ functions.php
@@ -94,3 +94,49 @@ function mytheme_setup_theme_supported_features() {
 add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
 
 
+function tapacode_client_setup_post_type() {
+
+    /*
+    * Labels for client custom post type
+    */
+    $labels = array(
+        'name'                  => _x( 'Clients', 'Post type general name', 'client' ),
+        'singular_name'         => _x( 'Client', 'Post type singular name', 'client' ),
+        'menu_name'             => _x( 'Clients', 'Admin Menu text', 'client' ),
+        'name_admin_bar'        => _x( 'Client', 'Add New on Toolbar', 'client' ),
+        'add_new'               => __( 'Add New', 'client' ),
+        'add_new_item'          => __( 'Add New client', 'client' ),
+        'new_item'              => __( 'New client', 'client' ),
+        'edit_item'             => __( 'Edit client', 'client' ),
+        'view_item'             => __( 'View client', 'client' ),
+        'all_items'             => __( 'All clients', 'client' ),
+        'search_items'          => __( 'Search clients', 'client' ),
+        'parent_item_colon'     => __( 'Parent clients:', 'client' ),
+        'not_found'             => __( 'No clients found.', 'client' ),
+        'not_found_in_trash'    => __( 'No clients found in Trash.', 'client' ),
+        'featured_image'        => _x( 'Client Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'client' ),
+        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'client' ),
+        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'client' ),
+        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'client' ),
+        'archives'              => _x( 'Client archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'client' ),
+        'insert_into_item'      => _x( 'Insert into client', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'client' ),
+        'uploaded_to_this_item' => _x( 'Uploaded to this client', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'client' ),
+        'filter_items_list'     => _x( 'Filter clients list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'client' ),
+        'items_list_navigation' => _x( 'Clients list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'client' ),
+        'items_list'            => _x( 'Clients list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'client' ),
+    );
+
+    //Register CPT client
+    register_post_type('tapacode_client',
+        array(
+            'labels'      => $labels,
+            'public'      => true,
+            'has_archive' => true,
+            'rewrite'     => array( 'slug' => 'client'),
+            'capability_type'    => 'post',
+            'supports'    => array( 'title', 'thumbnail', 'excerpt' ),
+            'show_in_rest' => true,
+        )
+    );
+}
+add_action( 'init', 'tapacode_client_setup_post_type' );
