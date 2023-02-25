65589f556ef42a0204a432c67eb87686e23c69e5
Joel Cormack <dgowellkomrak@gmail.com>
2023-02-25 11:30:12 -0600

    feature/ add switch between section on contact page

--- js/aspectus-switches.js
+++ js/aspectus-switches.js
@@ -49,6 +49,8 @@ jQuery(document).ready(function () {
 jQuery(document).ready(function () {
     //only run on contact page 1747 sectors
     if (jQuery("body").hasClass('page-id-1747')) {
+
+        // handle the accordian like dropdown office locations
         const switches = jQuery('.js-location__toggle');
         const submenus = jQuery('.js-location__dropdown');
         const locationSeparators = jQuery('.js-location__separator');
@@ -74,5 +76,34 @@ jQuery(document).ready(function () {
                 }
             })
         }
+
+        //hanlde the switches between the two different sections on the contact page
+        const officeSwitch = jQuery('.js-office-switch');
+        const formSwitch = jQuery('.js-contact-switch');
+        const formSection = jQuery('.js-contact-form__section');
+        const officeSection = jQuery('.js-locations__section');
+
+        //clicking the office switch will
+        //1. add active class to office switch
+        //2. remove actve class from contact form switch
+        //3. add active class to office locations section
+        //4. remove active class from contact form section
+        jQuery(officeSwitch).click(()=>{
+            if (jQuery(formSwitch).hasClass('active')){
+                jQuery(officeSwitch).addClass('active');
+                jQuery(formSwitch).removeClass('active');
+                jQuery(officeSection).addClass('active');
+                jQuery(formSection).removeClass('active');
+            }
+        });
+        //then we do the opposite for the other switch so it's like a toggle
+        jQuery(formSwitch).click(() => {
+            if (jQuery(officeSwitch).hasClass('active')) {
+                jQuery(formSwitch).addClass('active');
+                jQuery(officeSwitch).removeClass('active');
+                jQuery(formSection).addClass('active');
+                jQuery(officeSection).removeClass('active');
+            }
+        });
     }
 });
