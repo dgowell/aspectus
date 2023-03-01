0b410ceb01d5c81b94af04b502e710fcf01e5993
Joel Cormack <dgowellkomrak@gmail.com>
2023-03-01 17:49:23 -0600

    feature/add submenu first layer

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,83 +1,108 @@
-if (window.matchMedia('(min-width: 1000px)').matches) {
-    function openNav() {
-        //document.getElementsByClassName('js-main-nav')[0].style.height = '954px';
-        //document.getElementsByClassName('js-main-nav')[0].style.padding = '40px 40px 0';
-        //document.getElementsByClassName('header__column-line')[0].style.opacity = '1';
-        //document.getElementsByClassName('js-navbar__close-icon')[0].style.display = 'block';
-        //document.getElementsByClassName('js-navbar__burger-icon')[0].style.display = 'none';
-        //document.getElementsByClassName('js-navbar__burger-icon')[1].style.display = 'none';
-        //document.getElementsByClassName('js-navbar__logo--white')[0].style.display = 'none';
-        //document.getElementsByClassName('js-navbar__logo--black')[0].style.display = 'block';
-        document.getElementsByClassName('js-navbar')[0].classList.add('open');
-    }
-    function closeNav() {
-        //document.getElementsByClassName('js-main-nav')[0].style.height = '0';
-        //document.getElementsByClassName('js-main-nav')[0].style.padding = '0 40px 0';
-        //document.getElementsByClassName('header__column-line')[0].style.opacity = '0';
-        //document.getElementsByClassName('js-navbar__close-icon')[0].style.display = 'none';
-        document.getElementsByClassName('js-navbar')[0].classList.remove('open');
-
-        //check for certain pages
-        if (jQuery('body').hasClass('page-id-1735')) {
-            //document.getElementsByClassName('js-navbar__burger-icon')[1].style.display = 'block';
-        } else {
-            //document.getElementsByClassName('js-navbar__burger-icon')[0].style.display = 'block'
-            //document.getElementsByClassName('js-navbar__logo--white')[0].style.display = 'block';
-            //document.getElementsByClassName('js-navbar__logo--black')[0].style.display = 'none';
-        }
-    }
+function openNav() {
+    document.getElementsByClassName('js-navbar')[0].classList.add('open');
 }
-if (window.matchMedia('(max-width: 999px)').matches) {
-    function openNav() {
-        document.getElementsByClassName('js-main-nav')[0].style.height = '1000px';
-        document.getElementsByClassName('js-main-nav')[0].style.padding = '40px 40px 60px';
-        document.getElementsByClassName('js-navbar__close-icon')[0].style.display = 'block';
-        document.getElementsByClassName('js-navbar__burger-icon')[0].style.display = 'none';
-        document.getElementsByClassName('js-navbar__logo--white')[0].style.display = 'none';
-        document.getElementsByClassName('js-navbar__logo--black')[0].style.display = 'block';
-        document.getElementsByClassName('js-navbar')[0].classList.add('open');
-    }
-    function closeNav() {
-        document.getElementsByClassName('js-main-nav')[0].style.height = '0';
-        document.getElementsByClassName('js-main-nav')[0].style.padding = '0 40px 0';
-        document.getElementsByClassName('js-navbar__close-icon')[0].style.display = 'none';
-        document.getElementsByClassName('js-navbar__burger-icon')[0].style.display = 'block';
-        document.getElementsByClassName('js-navbar')[0].classList.remove('open');
-        //check for certain pages
-        if (jQuery('body').hasClass('page-id-1735')) {
-
-        } else {
-            document.getElementsByClassName('js-navbar__logo--white')[0].style.display = 'block';
-            document.getElementsByClassName('js-navbar__logo--black')[0].style.display = 'none';
-        }
-    }
+function closeNav() {
+    document.getElementsByClassName('js-navbar')[0].classList.remove('open');
+    //remove color the navbar grey
+    jQuery('.js-navbar').removeClass('highlight');
+    jQuery('.sub-menu.show').removeClass('show');
+    jQuery('.js-main-nav').removeClass('hide');
 }
+
+
+function displaySubmenu(e) {
+    //move main menu
+    jQuery('.js-main-nav').addClass('hide');
+    //get submenu
+
+    const submenu = jQuery(`.sub-menu:contains('${e.currentTarget.innerText}')`);
+    //display submenu
+    jQuery(submenu).addClass('show');
+
+    //color the navbar grey
+    jQuery('.js-navbar').addClass('highlight');
+
+}
+function hideSubmenu() {
+    console.log("hidesubmenu called");
+    //move main menu
+    jQuery('.js-main-nav').removeClass('hide');
+    //get submenu
+    jQuery('.sub-menu.show').removeClass('show');
+    //display submenu
+
+
+    //remove color the navbar grey
+    jQuery('.js-navbar').removeClass('highlight');
+}
+function handleBack() {
+
+    hideSubmenu();
+}
+
 jQuery(document).ready(function () {
+
+    /*
+    * Add the titles at the top of the sub menus in the main nav
+    */
+    const menuItems = jQuery('.menu-item-has-children');
+    jQuery.each(menuItems, function () {
+        const title = jQuery(this).find('a').first().text();
+        //add the title to the top of the menu
+        jQuery(this).find('li').first().before(`<h3 class="js-sub-menu__title">${title}</h3>`);
+    });
+
+
+
+    //ONLY RUN ON DESKTOP]
     if (window.matchMedia('(min-width: 1000px)').matches) {
-        //Function to add the titles at the top of the sub menus in the main nav
-        jQuery('.menu-item').hover(
+        /*
+        * Add the grey menu breaker for the sub sub menu
+        */
+        jQuery('.main-nav__container .sub-menu .menu-item-has-children').hover(
             function () {
-                const title = jQuery(this).find('a').first().text();
-                //add the title to the top of the menu
-                jQuery(this).find('li').first().before(`<h3 class="js-sub-menu__title">${title}</h3>`);
+                //find the line and increase the height to show it
+                jQuery('.header__column-line--second').first().height(543);
             },
             function () {
-                //remove the appended item
-                jQuery(this).find('.js-sub-menu__title').last().remove();
+                //decrease the height to hide it
+                jQuery('.header__column-line--second').first().height(0);
             }
         )
-        if (window.matchMedia('(min-width: 1000px)').matches) {
-            //Function to add the grey menu breaker for the sub sub menu
-            jQuery('.main-nav__container .sub-menu .menu-item-has-children').hover(
-                function () {
-                    //find the line and increase the height to show it
-                    jQuery('.header__column-line--second').first().height(543);
-                },
-                function () {
-                    //decrease the height to hide it
-                    jQuery('.header__column-line--second').first().height(0);
-                }
-            )
-        }
+    }//END DESKTOP
+
+    //ONLY RUN ON MOBILE
+    if (window.matchMedia('(max-width: 1000px)').matches) {
+
+        /*
+        * Remove a links in menu on mobile
+        */
+
+
+        jQuery('.menu-item-has-children>a').click((e) => {
+            e.preventDefault();
+            displaySubmenu(e);
+        });
+
+
+        /*
+        * 1.find the items that have children
+        * 2. attach fucntion
+        * 3. function moves main menu to the side and displays sub menu
+        * 4. displays the back button at the top, which does the reverse
+        *
+        */
+
+
+        const menuItems = jQuery('.menu-item-has-children');
+        jQuery.each(menuItems, function () {
+            const backName = 'Main menu';
+            const backLink = `<span class="sub-menu__backlink" onclick="handleBack()">< Back to <strong>${backName}</strong></span>`;
+            //add the title to the top of the menu
+            jQuery(this).find('h3').before(backLink);
+        });
+
     }
 });
+
+
